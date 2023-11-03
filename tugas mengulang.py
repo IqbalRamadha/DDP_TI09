bf3b4d62d607640899565ae6923b9cd673514120
IqbalRamadha <148035714+IqbalRamadha@users.noreply.github.com>
2023-11-03 08:45:51 +0700

    Add files via upload

---
+++ tugas mengulang.py
@@ -0,0 +1,103 @@
+# a = 0
+# while a<=3:
+#         print(a)
+#         a+=1
+
+# a = 0
+# while a<100:
+#         print(a,end=" ")
+#         a+=2
+
+# a = 0
+# while a<50:
+#         print("iqbaal")
+#         a+=1
+
+# a = 99
+# while a>0:
+#         print(a)
+#         a-=2
+
+# j = 1 
+# while j <=10:
+#     k=1
+#     while k <=10:
+#     print(j.k< end=" ")
+#     k+=1
+#     print()
+#     j+=1
+
+# for i in range(5,50):
+#     print  (i)
+
+# for i in range(100,0,-3):
+#     print (i)
+
+# for i in range(50):
+#     print("ti09")
+
+# for i in range(1,11):
+#     for j  in range(1,11):
+#         k=1*j
+#         print(k,end="  ")
+#     print()
+
+# i=1
+# while i<6:
+#     print(i)
+#     if  i==3:
+#         break
+#     i+=1
+
+
+#LATIHAN SOAL
+
+# SOAL NO1
+
+# numbers = (951, 402, 984, 651, 360, 69, 408, 319, 601, 485, 980, 507, 725, 
+# 547, 544, 615, 83, 165, 141, 501, 263, 617, 865, 575, 219, 390, 
+# 984, 592, 236, 105, 942, 941, 386, 462, 47, 418, 907, 344, 236, 
+# 375, 823, 566, 597, 978, 328, 615, 953, 345, 399, 162, 758, 219, 
+# 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217, 815, 
+# 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 
+# 742, 717, 958, 609, 842, 451, 688, 753, 854, 685, 93, 857, 440, 
+# 380, 126, 721, 328, 753, 470, 743, 527)
+
+
+# b = 0
+# while b<= len (numbers):
+#     if numbers [b]% 2 !=0:  
+#       print(numbers[b])
+#     if numbers[b]==553:
+#         break
+#     b+=1
+
+#SOAL NO2
+
+# count = 0
+# for index in range (1,20,2):
+#         count += index
+# print("1+3+5+7+9+11+15+17+19=",count)
+        
+
+# #SOAL NO3
+# string = " "
+# bar = 1
+
+# x = int (input("masukan angka= "))
+
+# #LOOPING BARIS
+# while bar <= x:
+#     kol=bar
+
+# #LOOPING KOLOM
+# while kol > 0:
+#     string = string + "*"
+#     kol = kol - 1
+# string = string + "\n"
+# bar = bar +1
+# print (string)
+    
+
+
+
